c72372a57b39cd8d2415435198de407a9b37184b
diogo <a2022148578@isec.pt>
2024-10-26 09:42:37 +0100

    Updated MainMenu Ui

--- MenuFit/src/main/java/pt/isec/ui/controllers/MainMenuController.java
+++ MenuFit/src/main/java/pt/isec/ui/controllers/MainMenuController.java
@@ -9,8 +9,11 @@ import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
-import javafx.stage.Stage;
+import pt.isec.model.meals.MealPlan;
 import pt.isec.model.users.User;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
 
 import java.io.IOException;
 
@@ -22,12 +25,37 @@ public class MainMenuController {
     public Label recipeTimeLabel;
     public ImageView recipeImage;
 
+    @FXML
+    private Label breakfastRecipeLabel;
+    @FXML
+    private Label lunchRecipeLabel;
+    @FXML
+    private Label dinnerRecipeLabel;
+
+    @FXML
+    private Label caloriesConsumedLabel; // Label para calorias consumidas
+    @FXML
+    private Label caloriesRemainingLabel; // Label para calorias restantes
+
+    //Propriedades calorias
+    private IntegerProperty caloriesConsumed = new SimpleIntegerProperty(100); // valor inicial
+    private IntegerProperty caloriesRemaining = new SimpleIntegerProperty(1900); // valor inicial
+
     private User user;
     SceneSwitcher sceneSwitcher;
-    
+
     public MainMenuController(){
         this.sceneSwitcher = new SceneSwitcher();
     }
+
+    @FXML
+    public void initialize() {
+        // Vinculação dos Labels para mostrar os valores de calorias (Contador de calorias)
+        caloriesConsumedLabel.textProperty().bind(caloriesConsumed.asString().concat("/2000"));
+        caloriesRemainingLabel.textProperty().bind(caloriesRemaining.asString().concat("/2000"));
+    }
+
+
     @FXML
     private void handleButtonAction(ActionEvent event) {
         /*try {
@@ -56,7 +84,20 @@ public class MainMenuController {
         //TODO mudar a receita em display quando a checkbox é selecionada
     }
 
+    private MealPlan mealPlan;
+
     public void initializeUser(User user) {
         this.user = user;
+
     }
+
+
+    // Métodos para atualizar os valores das calorias
+   /* public void setCaloriesConsumed(int calories) {
+        caloriesConsumed.set(calories);
+    }
+
+    public void setCaloriesRemaining(int calories) {
+        caloriesRemaining.set(calories);
+    }*/
 }
