0fe795f24d0341e44ff19a8a6fca36e13d274dbe
Júlio Pacheco <juliodryfish@protonmail.com>
2024-12-08 19:45:19 +0000

    Updated Sample Test class

--- MenuFit/src/test/java/SampleTest.java
+++ MenuFit/src/test/java/SampleTest.java
@@ -4,7 +4,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class SampleTest {
     @Test
     void testAddition() {
-        int result = 5;
-        assertEquals(5, result, "Result should be 5.");
+        int result = 2 + 3;
+        assertEquals(5, result, "2 + 3 should be 5.");
+    }
+
+    @Test
+    void testSubtraction() {
+        int result = 3 - 2;
+        assertEquals(1, result, "3 - 2 should be 1.");
     }
 }
