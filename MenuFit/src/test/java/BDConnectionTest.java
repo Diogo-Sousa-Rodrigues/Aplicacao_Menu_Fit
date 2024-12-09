4a7e2be51823ab73272d1cf96df4fda40ab9c28c
Tiago Fernandes <165323272+Tiago-FCF@users.noreply.github.com>
2024-12-09 22:53:15 +0000

    [Name change] BDConnectionTest.java

--- MenuFit/src/test/java/BDConnectionTest.java
+++ MenuFit/src/test/java/BDConnectionTest.java
@@ -10,7 +10,7 @@ import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class BDManagerTest {
+class BDConnectionTest {
     private BDManager bdManager;
 
     @BeforeEach
