ca7749822c8489a5ca84306c3aba0ce0bef0e035
diogo <a2022148578@isec.pt>
2024-12-09 21:18:35 +0000

    Unit test for database connection

---
+++ MenuFit/src/test/java/BDManagerTest.java
@@ -0,0 +1,37 @@
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pt.isec.persistence.BDManager;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BDManagerTest {
+    private BDManager bdManager;
+
+    @BeforeEach
+    void setUp() {
+        bdManager = new BDManager();
+    }
+
+    @AfterEach
+    void tearDown() {
+        bdManager.closeConnection();
+    }
+
+    @Test
+    void testConnection() {
+        assertNotNull(bdManager, "BDManager não foi inicializado corretamente.");
+
+        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:src/Base_de_Dados_MenuFit.db")) {
+            assertNotNull(conn, "A conexão com o banco de dados falhou.");
+            System.out.println("Teste de conexão com o banco de dados: SUCESSO");
+        } catch (SQLException e) {
+            fail("Erro ao conectar ao banco de dados: " + e.getMessage());
+        }
+    }
+}
